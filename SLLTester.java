839fc1ac83a234b4ed2e75187fff0ffb05feab1a
hregahego <95511503+hregahego@users.noreply.github.com>
2024-11-21 03:56:16 +0000

    finished program 11/20/24

--- SLLTester.java
+++ SLLTester.java
@@ -1,5 +1,89 @@
-class SLLTester {
-  public static void main(String[] args) {
+//Steven Wang, Pd. 3
 
-  } // end main
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+class SLLTester {
+    public static void main(String[] args) {
+        SuperLinkedList list1 = new SuperLinkedList();
+        SuperLinkedList list2 = new SuperLinkedList();
+        SuperLinkedList list3 = new SuperLinkedList();
+ 
+ 
+        list1.add("A");
+        list1.add("B");
+        list1.add("C");
+        list1.add("D");
+        list1.add("E");
+ 
+ 
+        list2.add("F");
+        list2.add("G");
+        list2.add("H");
+        list2.add("I");
+        list2.add("J");
+ 
+ 
+        list3.add("K");
+        list3.add("L");
+        list3.add("M");
+        list3.add("N");
+        list3.add("O");
+        list3.add("K");
+        list3.add("K");
+        list3.add("N");
+ 
+        
+        System.out.println("This is list 1: \n" + list1.toString());
+        System.out.println("This is list 2: \n" + list2.toString());
+        System.out.println("This is list 3: \n" + list3.toString());
+ 
+ 
+        boolean removed = list1.removeVowels();
+        System.out.println("\n\nlist1 removed vowels? " + removed);
+        System.out.println(list1.toString());
+ 
+ 
+ 
+ 
+        removed = list2.removeConsonants();
+        System.out.println("\n\nlist2 removed consonants? " + removed);
+        System.out.println(list2.toString());
+ 
+ 
+ 
+ 
+        System.out.println("\n\nlist3 remove duplicates");
+        LinkedList<String> noDuplicates = list3.removeDuplicates();
+        list3 = new SuperLinkedList(noDuplicates);
+        ListIterator<String> it = noDuplicates.listIterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        } //loop through list
+ 
+ 
+        System.out.println("\n\nlist3 concatenations");
+        System.out.println(list3.concatenateStrings());
+       
+        System.out.println("\n\nlist 3 mixed with list1");
+        LinkedList<String> mixed = new LinkedList<String>();
+        LinkedList<String> linked1 = list1;
+        mixed = list3.mix(linked1);
+        it = mixed.listIterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        } //loop through list
+ 
+ 
+        System.out.println("\n\nFinal lists: ");
+        System.out.println("List 1: " + list1.toString());
+        System.out.println("List 2: " + list2.toString());
+        System.out.println("List 3: " + list3.toString());
+ 
+ 
+ 
+ 
+ 
+    } // end main
 } // end class
--- SuperLinkedList.java
+++ SuperLinkedList.java
@@ -1,31 +1,127 @@
+//Steven Wang, Pd. 3
+
 import java.util.LinkedList;
-import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.HashSet;
 
-public class SuperLinkedList extends LinkedList<String> {
-    public boolean removeVowels() {
-        return false;
-    }
 
-    public boolean removeConsonants() {
-        return false;
-    }
+class SuperLinkedList extends LinkedList<String> {
 
-    public LinkedList<String> removeDuplicates() {
-        return null;
-    }
+   public SuperLinkedList() {
+       super();
+   } //end default constructor
 
-    public LinkedList<String> concatenateStrings() {
 
-        return null;
-    }
+   public SuperLinkedList(LinkedList<String> init) {
+       super();
+       for (String i : init) {
+           super.add(i);
+       }
+   } //end constructor that takes LinkedList as input
+  
+   public boolean removeVowels() {
+       ListIterator<String> it = super.listIterator();
+       boolean removed = false;
+       String vowels = "aeiou";
+       while(it.hasNext()) {
+           String current = it.next();
+           if (vowels.contains(current.toLowerCase())) {
+               it.remove();
+               removed = true;
+           } //checks if current letter is a vowel - if so, removes
+       } //loop through all the letters
 
-    public LinkedList<String> mix(LinkedList<String> list2) {
 
-        return null;
-    }
+       return removed;
+   } //end removeVowels
 
-    public String toString() {
 
-        return "";
-    }
-} // end SuperLinkedList
+   public boolean removeConsonants() {
+       ListIterator<String> it = super.listIterator();
+       boolean removed = false;
+       String consonants = "bcdfghjklmnpqrstvwxyz";
+       while(it.hasNext()) {
+           String current = it.next();
+           if (consonants.contains(current.toLowerCase())) {
+               it.remove();
+               removed = true;
+           } //checks if current letter is a consonant - if so, removes
+       } //loop through all the letters
+      
+       return removed;
+   } //end removeConsonants
+
+
+   public LinkedList<String> removeDuplicates() {
+       HashSet<String> seen = new HashSet<String>();
+       ListIterator<String> it = super.listIterator();
+       LinkedList<String> unique = new LinkedList<String>();
+
+
+       while(it.hasNext()) {
+           String current = it.next();
+           if (!seen.contains(current)) {
+               unique.add(current);
+           } //checks if current is in the set - if no, then add to unique list
+           seen.add(current);
+       } //loop through all letters
+
+
+       return unique;
+   } //end removeDuplicates
+
+
+   public LinkedList<String> concatenateStrings() {
+       LinkedList<String> result = new LinkedList<String>();
+       for (int i = 0; i < super.size(); i++) {
+           ListIterator<String> it = super.listIterator();
+           String concat = "";
+           for (int j = 0; j <= i; j++) {
+               String current = it.next();
+               concat += current;
+           } //loop through beginning to current # of concatenation
+           result.add(concat);
+       } //loop through total # of concatenations
+       return result;
+   } //end concatenateStrings
+
+
+   public LinkedList<String> mix(LinkedList<String> list2) {
+
+
+       ListIterator<String> it1 = super.listIterator();
+       ListIterator<String> it2 = list2.listIterator();
+       LinkedList<String> result = new LinkedList<String>();
+
+
+       while (it1.hasNext() || it2.hasNext()) {
+           if (it1.hasNext()) {
+               result.add(it1.next());
+           } //if there are still items remaining in this list, add
+           if (it2.hasNext()) {
+               result.add(it2.next());
+           } //if there are still items remaining in the input list, add
+       } //loop while either this list or the input list have items left
+
+
+       return result;
+
+
+   } //end mix
+
+
+   public String toString() {
+       String result = "";
+       ListIterator<String> it = super.listIterator();
+       while (it.hasNext()) {
+           result += it.next() + ", ";
+       } //add current letter and comma to result
+
+
+       return result;
+
+
+   } //end toString
+
+
+} //end class
